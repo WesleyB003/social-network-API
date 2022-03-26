2a29885c962dea5f896946c87f7128313025c6b2
Wesley <wabryant97@gmail.com>
2022-03-26 18:17:00 -0400

    made reactionModelSchema in ReactionModel

--- models/ReactionModel.js
+++ models/ReactionModel.js
@@ -1,1 +1,32 @@
 const {Schema, Types} =require('mongoose');
+
+const moment = require('moment');
+
+const reactionModelSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId, 
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        // Found formating for date on momentjs.com
+        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm:a')
+    }
+}, {
+    toJSON: {
+        getters: true
+    },
+    id: false
+});
+
+module.exports = reactionModelSchema;
